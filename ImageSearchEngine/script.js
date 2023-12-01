8a2cb0952739d109763cac3fa5d718339f6bdf97
ItzAxBRealm <140930458+ItzAxBRealm@users.noreply.github.com>
2023-12-01 18:08:22 +1100

    Add files via upload

---
+++ ImageSearchEngine/script.js
@@ -0,0 +1,51 @@
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult = document.getElementById('search-result');
+const showMoreBtn = document.getElementById('show-more-btn');
+
+let keyword = "";
+let page = 1;
+const accessKey = "LLe5itips3duf723Z8gD9fHe3AtAwJAWFG4sHlQ01xk"
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+
+    results.map((result) => {
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+
+    showMoreBtn.style.display = 'block';
+
+    searchResult.style.background = "var(--clr-4)"
+    searchResult.style.padding = "60px 100px"
+    searchResult.style.border = "8px solid var(--clr-5)"
+    searchResult.style.borderRadius = "8px "
+}
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMoreBtn.addEventListener('click', () => {
+    page++;
+    searchImages();
+})
